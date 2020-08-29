5cb3a5c96100d724894bfc7cbf914a18dd5aa7bc
Yury Chistov <yurachistic@gmail.com>
2020-08-29 10:21:49 +0200

    Add views

--- generalpages/views.py
+++ generalpages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+     context = {}
+     return render(request, 'generalpages/home.html', context)
+
+def charities(request):
+     context = {}
+     return render(request, 'generalpages/charities.html', context)
+
+def contacts(request):
+      context = {}
+      return render(request, 'generalpages/contacts.html', context)
