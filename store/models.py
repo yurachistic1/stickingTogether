e97dbf88e25c0679729528ec881c01df3b31a709
Yury Chistov <yurachistic@gmail.com>
2020-09-05 17:12:20 +0100

    Create and register Sticker model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Sticker(models.Model):
+    BEIRUTREDCROSS = 'BRC'
+    CAUSES_CHOICES = [
+        (BEIRUTREDCROSS, 'Beirut Red Cross'),
+    ]
+
+    name = models.CharField(max_length=30, unique=True)
+    price = models.FloatField()
+    description = models.TextField(default="", blank=True)
+    artist = models.CharField(max_length=30)
+    singapore_stock = models.IntegerField()
+    uk_stock = models.IntegerField()
+    cause = models.CharField(max_length=5, choices=CAUSES_CHOICES)
+
+    def __str__(self):
+        return self.name
