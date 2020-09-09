740a4c54adfc3b7baaa61e1913df63aef54be40a
Yury Chistov <yurachistic@gmail.com>
2020-09-09 11:23:39 +0100

    Update Sticker model

--- store/models.py
+++ store/models.py
@@ -7,6 +7,7 @@ def image_directory_path(instance, filename):
 # Create your models here.
 
 class Sticker(models.Model):
+
     BEIRUTREDCROSS = 'BRC'
     CAUSES_CHOICES = [
         (BEIRUTREDCROSS, 'Beirut Red Cross'),
@@ -15,16 +16,29 @@ class Sticker(models.Model):
     name = models.CharField(max_length=30, unique=True)
     price = models.FloatField(verbose_name="Price (£)")
     description = models.TextField(default="", blank=True)
+    dimensions = models.CharField(verbose_name="Dimensions (A x B cm)", null=True, max_length=10)
     artist = models.CharField(max_length=30)
     singapore_stock = models.IntegerField()
     uk_stock = models.IntegerField()
     cause = models.CharField(max_length=5, choices=CAUSES_CHOICES)
     image_1 = models.ImageField(
+        verbose_name="Primary Image",
         upload_to=image_directory_path, 
         null=True, 
         blank=True
     )
 
-    def __str__(self):
-        return self.name
+    image_2 = models.ImageField(
+        verbose_name="Secondary Image",
+        upload_to=image_directory_path, 
+        null=True, 
+        blank=True
+    )
 
+    ordering = models.IntegerField(verbose_name="Order of appearance(lowest first)", default=1)
+
+    class Meta:
+        ordering = ['ordering']
+
+    def __str__(self):
+        return ' '.join[self.name, "Ordering:", str(self.ordering)]
