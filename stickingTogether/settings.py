2d5ff069fab348884d0b19a7f7d3259aafd0c3b8
Yury Chistov <yurachistic@gmail.com>
2020-09-13 00:50:30 +0100

    add stackoverflow solution

--- stickingTogether/settings.py
+++ stickingTogether/settings.py
@@ -153,3 +153,5 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 django_heroku.settings(locals())
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
