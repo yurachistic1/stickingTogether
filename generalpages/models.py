454a47474322774090f536f8f555b75867e1f581
Yury Chistov <yurachistic@gmail.com>
2020-09-12 13:31:16 +0100

    Add inquiry model

--- generalpages/models.py
+++ generalpages/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+
+class Inquiry(models.Model):
+    
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    datetime = models.DateTimeField(default=timezone.now)
+    status = models.BooleanField(verbose_name="Completed", default=False)
+
+    def __str__(self):
+        active = 'Completed.' if self.status else 'Active.'  
+        return  active + " Subject: " + self.subject + (self.datetime).strftime(" (%x - %X)")
+
+    class Meta:
+        ordering = ['datetime']
