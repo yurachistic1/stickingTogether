4c3584e0ba1d2dae9178e6a70f68cbe54ab3d3a6
Yury Chistov <yurachistic@gmail.com>
2020-09-12 23:07:22 +0100

    Integrate s3 storage

--- stickingTogether/settings.py
+++ stickingTogether/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     'generalpages',
     
     #Cleanup
-    'django_cleanup.apps.CleanupConfig'
+    'django_cleanup.apps.CleanupConfig',
+
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,15 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY= os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME= os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+AWS_S3_REGION_NAME = 'eu-west-2' #change to your region
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
