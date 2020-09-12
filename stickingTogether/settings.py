0a0bb281b9457ef631b87d61cc5b928c36e0011c
Yury Chistov <yurachistic@gmail.com>
2020-09-12 23:39:13 +0100

    Update settings

--- stickingTogether/settings.py
+++ stickingTogether/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ksc39&k_l$x24sf9*5t@f8_k_xrdi3&n-n#5x%ig8wo)%f0y&a'
+SECRET_KEY = os.environ.get('SECRET_KEY') #'ksc39&k_l$x24sf9*5t@f8_k_xrdi3&n-n#5x%ig8wo)%f0y&a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sticking-together-app.herokuapp.com']
 
 
 # Application definition
