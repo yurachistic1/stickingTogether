400498dea7c6ff416fb608433eec5294398df6e9
Yury Chistov <yurachistic@gmail.com>
2020-09-12 21:16:56 +0100

    Move update counter script to a sep file

---
+++ generalpages/static/generalpages/js/updateCartCounter.js
@@ -0,0 +1,10 @@
+function getNoOfItems(){
+    let counter = document.getElementById('bsk-count')
+    cart = JSON.parse(getCookie('cart'))
+    total = 0
+    for (const value of Object.values(cart)){
+        total += value['quantity']
+    }
+
+    counter.innerHTML = total
+}
